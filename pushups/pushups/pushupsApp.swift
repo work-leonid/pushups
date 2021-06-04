e4c485ef8bc2c692d236cb7d2805cca10bcfdb88
work-leonid <work.leonid@gmail.com>
2021-06-04 15:45:23 +0300

    Initial

---
+++ pushups/pushups/pushupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushupsApp.swift
+//  pushups
+//
+//  Created by Leonid Nazarov on 04.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct pushupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
